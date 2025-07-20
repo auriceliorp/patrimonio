f2f7ca5c38021e100aa669468508c4e7944f49b8
auriceliorp <auriceliorp@gmail.com>
2025-07-20 14:16:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-<?
-
+<?php
 //
-//	Sistema: Bens patrimoniais
-//	Modulo: Principal
-//	Concluído em 25/08/2003
-//	Autor: Adil D. Pinto Jr.
-//	Embrapa Agroindustria de Alimentos
+// Sistema: Bens patrimoniais
+// Modulo: Principal
+// ConcluÃ­do em 25/08/2003
+// Autor: Adil D. Pinto Jr.
+// Embrapa Agroindustria de Alimentos
 //
 
 session_start();
@@ -16,53 +15,44 @@ require("fcs-gerais.php");
 require("./include/patrimonio.conf");
 mc_simples();
 
-if ( $_GET[tipo] == 'aceita' ) {
-	echo "<frameset rows='26%,74%' border=0 name='geral'>";
-	echo "<frame src='../cabecalho.php?titulo=Bens Patrimoniais - $VAR_Versao' name='cabecalho'>";
-		echo "<frameset cols='20%,80%'>";
-		echo "<frame src='./bem-menu.php' name='menu'>";
-		echo "<frame src='./bem-pesquisa.php?tipo=transf' name='dados'>";
-	echo "</frameset>";
-	echo "</frameset>";
+if ( $_GET['tipo'] == 'aceita' ) {
+    echo "<frameset rows='26%,74%' border=0 name='geral'>";
+    echo "<frame src='../cabecalho.php?titulo=Bens Patrimoniais - $VAR_Versao' name='cabecalho'>";
+    echo "<frameset cols='20%,80%'>";
+    echo "<frame src='./bem-menu.php' name='menu'>";
+    echo "<frame src='./bem-pesquisa.php?tipo=transf' name='dados'>";
+    echo "</frameset>";
+    echo "</frameset>";
 
-} elseif ( $_GET[tipo] == 'transf-pendente' ) {
-	echo "<frameset rows='26%,74%' border=0 name='geral'>";
-	echo "<frame src='../cabecalho.php?titulo=Bens Patrimoniais - $VAR_Versao' name='cabecalho'>";
-		echo "<frameset cols='20%,80%'>";
-		echo "<frame src='./bem-menu.php' name='menu'>";
-		echo "<frame src='./bem-pesquisa.php?tipo=transf-pendente' name='dados'>";
-	echo "</frameset>";
-	echo "</frameset>";
+} elseif ( $_GET['tipo'] == 'transf-pendente' ) {
+    echo "<frameset rows='26%,74%' border=0 name='geral'>";
+    echo "<frame src='../cabecalho.php?titulo=Bens Patrimoniais - $VAR_Versao' name='cabecalho'>";
+    echo "<frameset cols='20%,80%'>";
+    echo "<frame src='./bem-menu.php' name='menu'>";
+    echo "<frame src='./bem-pesquisa.php?tipo=transf-pendente' name='dados'>";
+    echo "</frameset>";
+    echo "</frameset>";
 
-} elseif ( $_GET[tipo] == 'termo-pendente' ) {
-	echo "<frameset rows='26%,74%' border=0 name='geral'>";
-	echo "<frame src='../cabecalho.php?titulo=Bens Patrimoniais - $VAR_Versao' name='cabecalho'>";
-		echo "<frameset cols='20%,80%'>";
-		echo "<frame src='./bem-menu.php' name='menu'>";
-		echo "<frame src='./bem-rel1.php' name='dados'>";
-	echo "</frameset>";
-	echo "</frameset>";
+} elseif ( $_GET['tipo'] == 'termo-pendente' ) {
+    echo "<frameset rows='26%,74%' border=0 name='geral'>";
+    echo "<frame src='../cabecalho.php?titulo=Bens Patrimoniais - $VAR_Versao' name='cabecalho'>";
+    echo "<frameset cols='20%,80%'>";
+    echo "<frame src='./bem-menu.php' name='menu'>";
+    echo "<frame src='./bem-rel1.php' name='dados'>";
+    echo "</frameset>";
+    echo "</frameset>";
 
-} elseif ( $_GET[tipo] == 'aliena-pendente' ) {
-	echo "<frameset rows='26%,74%' border=0 name='geral'>";
-	echo "<frame src='../cabecalho.php?titulo=Bens Patrimoniais - $VAR_Versao' name='cabecalho'>";
-		echo "<frameset cols='20%,80%'>";
-		echo "<frame src='./bem-menu.php' name='menu'>";
-		echo "<frame src='./bem-pesquisa.php?tipo=aliena-conf' name='dados'>";
-	echo "</frameset>";
-	echo "</frameset>";
+} elseif ( $_GET['tipo'] == 'aliena-pendente' ) {
+    echo "<frameset rows='26%,74%' border=0 name='geral'>";
+    echo "<frame src='../cabecalho.php?titulo=Bens Patrimoniais - $VAR_Versao' name='cabecalho'>";
+    echo "<frameset cols='20%,80%'>";
+    echo "<frame src='./bem-menu.php' name='menu'>";
+    echo "<frame src='./bem-pesquisa.php?tipo=aliena-conf' name='dados'>";
+    echo "</frameset>";
+    echo "</frameset>";
 
 } else {
-	echo "<frameset rows='26%,74%' border=0 name='geral'>";
-	echo "<frame src='../cabecalho.php?titulo=Bens Patrimoniais - $VAR_Versao' name='cabecalho'>";
-		echo "<frameset cols='20%,80%'>";
-		echo "<frame src='./bem-menu.php' name='menu'>";
-		echo "<frame src='./bem-meusbens.php?user=1' name='dados'>";
-	echo "</frameset>";
-	echo "</frameset>";
-
+    mc_simples();  // Aqui estava mr_simples(), corrigido para mc_simples()
 }
-
-mr_simples();
 
 ?>
