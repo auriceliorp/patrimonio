96a8db4876cd9e8b5c6f1a7629809543f3f66090
auriceliorp <auriceliorp@gmail.com>
2025-07-21 16:33:41 -0300

    Update fcs-gerais.php

--- fcs-gerais.php
+++ fcs-gerais.php
@@ -3,9 +3,8 @@
 // Funções gerais do sistema
 //
 
-// Verifica se a função já existe antes de declarar
+// Função para conectar ao banco de dados
 if (!function_exists('abre_banco')) {
-    // Função para conectar ao banco de dados
     function abre_banco($banco, $usuario, $servidor, $senha) {
         $conn = mysqli_init();
         mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
@@ -19,49 +18,27 @@ if (!function_exists('abre_banco')) {
 }
 
 // Função para tratar erros
-function trata_erro($mensagem) {
-    echo "<CENTER><BR>";
-    echo "<H3>Erro</H3>";
-    echo $mensagem . "<BR>";
-    echo "</CENTER><BR>";
-    exit;
+if (!function_exists('trata_erro')) {
+    function trata_erro($mensagem) {
+        echo "<center><br><h3>Erro</h3>$mensagem<br></center><br>";
+        exit;
+    }
 }
 
-// Função para formatar data
-function troca_data($data) {
-    if (empty($data)) return "";
-    $partes = explode("-", $data);
-    return $partes[2] . "/" . $partes[1] . "/" . $partes[0];
+// Funções de data
+if (!function_exists('troca_data')) {
+    function troca_data($data) {
+        if (empty($data)) return "";
+        $partes = explode("-", $data);
+        return $partes[2] . "/" . $partes[1] . "/" . $partes[0];
+    }
 }
 
-// Função para formatar data para o banco
-function troca_data_mysql($data) {
-    if (empty($data)) return "";
-    $partes = explode("/", $data);
-    return $partes[2] . "-" . $partes[1] . "-" . $partes[0];
+if (!function_exists('troca_data_mysql')) {
+    function troca_data_mysql($data) {
+        if (empty($data)) return "";
+        $partes = explode("/", $data);
+        return $partes[2] . "-" . $partes[1] . "-" . $partes[0];
+    }
 }
-
-// Função para cabeçalho simples
-function mc_simples() {
-    echo "<html>";
-    echo "<head>";
-    echo "<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>";
-    echo "</head>";
-    echo "<body bgcolor='#FFFFFF'>";
-}
-
-// Função para cabeçalho completo
-function mc_completo($titulo) {
-    echo "<html>";
-    echo "<head>";
-    echo "<title>$titulo</title>";
-    echo "<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>";
-    echo "</head>";
-    echo "<body bgcolor='#FFFFFF'>";
-}
-
-// Função para rodapé
-function mr_rodape() {
-    echo "</body>";
-    echo "</html>";
-} 
+?> 
